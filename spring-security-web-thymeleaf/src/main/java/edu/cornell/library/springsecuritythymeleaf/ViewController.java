e7bea262674e0d5ff92d0d8b873bd2722ec0ff7f
John Fereira <jaf30@cornell.edu>
2021-11-03 12:47:46 -0400

    initial version

---
+++ spring-security-web-thymeleaf/src/main/java/edu/cornell/library/springsecuritythymeleaf/ViewController.java
@@ -0,0 +1,18 @@
+package edu.cornell.library.springsecuritythymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping({ "/index", "/" })
+    public String index() {
+        return "index";
+    }
+}
