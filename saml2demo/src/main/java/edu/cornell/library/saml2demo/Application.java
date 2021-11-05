4ca7e6aba52e0c40207ae2dc050a3d5c6ee4a08a
John Fereira <jaf30@cornell.edu>
2021-11-05 09:54:32 -0400

    adding saml2demo

---
+++ saml2demo/src/main/java/edu/cornell/library/saml2demo/Application.java
@@ -0,0 +1,19 @@
+package edu.cornell.library.saml2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication 
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	} 
+
+}
+
