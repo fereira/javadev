3bb166a57ec10094f33678b98f4cdd3eaf6fda45
John Fereira <jaf30@cornell.edu>
2022-02-02 09:36:46 -0500

    added restlet jar

--- Misc/install-extra-lib.sh
+++ Misc/install-extra-lib.sh
@@ -1,8 +1,6 @@
-# DLESETools.jar
-# orcid-java-client-0.12.2.jar
-# org.restlet.jar
-# virt_jena2.jar
-# virtjdbc4.jar
+#!/bin/sh
 mvn -U install:install-file -Dfile=./extralib/orcid-java-client-0.12.2.jar -DgroupId=edu.cornell.library.misc -DartifactId=orcid-java-client -Dversion=0.12.2 -Dpackaging=jar -DgeneratePom=true
 
 mvn -U install:install-file -Dfile=./extralib/DLESETools.jar -DgroupId=edu.cornell.library.misc -DartifactId=DLESETools -Dversion=0.1.0 -Dpackaging=jar -DgeneratePom=true
+
+mvn -U install:install-file -Dfile=./extralib/org.restlet.jar -DgroupId=edu.cornell.library.misc -DartifactId=org.restlet -Dversion=2.3.0 -Dpackaging=jar -DgeneratePom=true
