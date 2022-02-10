687edac108ab19a5ffbfe46d83ec3e3ecd75a290
John Fereira <jaf30@cornell.edu>
2022-02-10 09:09:00 -0500

    fix version of xml-apis

--- Misc/src/main/java/edu/cornell/library/misc/jena/UriTest.java
+++ Misc/src/main/java/edu/cornell/library/misc/jena/UriTest.java
@@ -7,32 +7,33 @@ import org.apache.jena.shared.InvalidPropertyURIException;
 
 public class UriTest {
 
-	public UriTest() {
-		// TODO Auto-generated constructor stub
-	}
+    public UriTest() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		 String uriStr = "http://foo.bar/123";
-		 
-		String localName = uriStr.substring(uriStr.lastIndexOf("/") + 1);
-		//if local name is only numbers, this is not a valid uri for a property
-		// ResourceFactory.createProperty will throw an exception so don't call it
-		// and just return false;
-		
-		if(localName.matches("\\d+")) {
-			System.out.println("local name is all digits");
-		} else {
-			System.out.println("localName is valid");
-		}
-		createResource(uriStr);
-		try {
-		   System.out.println(createProperty(uriStr));
-		} catch (InvalidPropertyURIException ex) {
-			System.out.println("uri is invalid");	
-		}
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String uriStr = "http://foo.bar/123";
+
+        String localName = uriStr.substring(uriStr.lastIndexOf("/") + 1);
+        // if local name is only numbers, this is not a valid uri for a property
+        // ResourceFactory.createProperty will throw an exception so don't call it
+        // and just return false;
+
+        if (localName.matches("\\d+")) {
+            System.out.println("local name is all digits");
+        } else {
+            System.out.println("localName is valid");
+        }
+        createResource(uriStr);
+        try {
+            System.out.println(createProperty(uriStr));
+        } catch (InvalidPropertyURIException ex) {
+            System.out.println("uri is invalid");
+        }
+        System.out.println("done");
+    }
 
 }
