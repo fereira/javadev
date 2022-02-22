a1d9000a2fb8f6a620c335d58baba0c2c71239af
John Fereira <jaf30@cornell.edu>
2022-02-22 13:09:14 -0500

    print nodes before merge

--- Misc/src/main/java/edu/cornell/library/misc/json/JsonMergeTest.java
+++ Misc/src/main/java/edu/cornell/library/misc/json/JsonMergeTest.java
@@ -16,18 +16,25 @@ public class JsonMergeTest {
 	
 	ObjectMapper mapper = new ObjectMapper();
 
-    ObjectNode node1 = mapper.createObjectNode();
-    
+    ObjectNode node1 = mapper.createObjectNode(); 
     
     node1.put("name", "Sriram");
     node1.put("age", 2);
     node1.put("hobby", "painting");
+    System.out.println("node 1");
+    System.out.println(node1.toPrettyString());
     
     ObjectNode node2 = mapper.createObjectNode();
     node2.put("name", "Sriram");
+    node2.put("age", 30);
     node2.put("hobby", "Singing");
     node2.put("extraField", "read all about it");
-    ArrayNode arrayNode = mapper.createArrayNode();
+    System.out.println("node 2");
+    System.out.println(node2.toPrettyString());
+    
+    /**
+     * ArrayNode arrayNode = mapper.createArrayNode();
+     *
     ObjectNode node3 = mapper.createObjectNode();
     ObjectNode node4 = mapper.createObjectNode();
     node3.put("keyword", "one");
@@ -36,10 +43,11 @@ public class JsonMergeTest {
     arrayNode.add(node4);
      
     node2.put("keywordArray1", arrayNode);
+    */
      
     
-    System.out.println(node2.toString());
     JsonNode merged = merge(node1, node2) ;
+    System.out.println("merged");
     System.out.println(merged.toString());
   }
   
@@ -51,7 +59,7 @@ public class JsonMergeTest {
 	        String fieldName = fieldNames.next();
 	        System.out.println("fieldName: "+fieldName);
 	        JsonNode jsonNode = mainNode.get(fieldName);
-	        System.out.println("main node type: "+ mainNode.getNodeType());
+	        //System.out.println("main node type: "+ mainNode.getNodeType());
 	        // if field exists and is an embedded object
 	        if (jsonNode != null && jsonNode.isObject()) {
 	        	System.out.println("recursive merge"); 
@@ -75,7 +83,7 @@ public class JsonMergeTest {
 	    	if (! mainNode.has(updateFieldName)) {
 	    		System.out.println("Adding field: "+ updateFieldName);
 	    		JsonNode newNode = updateNode.get(updateFieldName);
-	    		System.out.println("type: "+newNode.getNodeType());
+	    		//System.out.println("type: "+newNode.getNodeType());
 	    	}
 	    }
 
