55a21256bee2f76a65787d7cbfaa58099800495a
John Fereira <jaf30@cornell.edu>
2022-03-25 10:17:07 -0400

    ??

--- LuceneDemo/scripts/LuceneSearch.sh
+++ LuceneDemo/scripts/LuceneSearch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 BUILD_DIR=/cul/src/javadev/LuceneDemo
-export CLASSPATH=`mvn -f ../pom.xml -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath edu.cornell.library.lucene.LuceneSearch" `
-# echo $CLASSPATH
+export CLASSPATH=`mvn -f ../pom.xml -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath" `
 java $OPTS -classpath $CLASSPATH edu.cornell.library.lucene.LuceneSearch $*
