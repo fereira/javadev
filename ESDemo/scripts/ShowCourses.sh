5cfe64657551635159d8b28461cdef5bd0aa4e0b
John Fereira <jaf30@cornell.edu>
2022-03-25 10:19:09 -0400

    updates to Java11 demo

---
+++ ESDemo/scripts/ShowCourses.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BUILD_DIR=/cul/src/javadev/ESDemo
+export CLASSPATH=`mvn -f ../pom.xml -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath" `
+java $OPTS -classpath $CLASSPATH edu.cornell.library.esdemo.ShowCourses $*
---
+++ ESDemo/scripts/run-elasticsearch.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/cul/app/elasticsearch-7.8.1/bin/elasticsearch -d -v -p pid
