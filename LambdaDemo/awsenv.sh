f1cde5d7a6c11125eb9193662c29f974f018a03a
John Fereira <jaf30@cornell.edu>
2022-04-06 10:07:49 -0400

    Delete awsenv.sh

--- LambdaDemo/awsenv.sh
+++
@@ -1,4 +0,0 @@
-export AWS_REGION=us-east-1
-export AWS_ACCESS_KEY_ID=AKIAZG4AJBJPHAU4ENGZ 
-export AWS_SECRET_ACCESS_KEY=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 
-export AWS_SQS_ENDPOINT=https://sqs.us-east-1.amazonaws.com
