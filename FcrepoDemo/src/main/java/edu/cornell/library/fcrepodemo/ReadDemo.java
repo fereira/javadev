7f2bac21e9ab54f86d4fe47d524f81b7a5588c3a
John Fereira <jaf30@cornell.edu>
2022-04-07 09:52:23 -0400

    read properties in ReadDemo

--- FcrepoDemo/src/main/java/edu/cornell/library/fcrepodemo/ReadDemo.java
+++ FcrepoDemo/src/main/java/edu/cornell/library/fcrepodemo/ReadDemo.java
@@ -1,18 +1,25 @@
 package edu.cornell.library.fcrepodemo;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 import org.fcrepo.client.FcrepoClient;
 import org.fcrepo.client.FcrepoOperationFailedException;
 import org.fcrepo.client.FcrepoResponse;
 import org.fcrepo.client.GetBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 public class ReadDemo {
+    
+    @Value("${fcrepo.uri}")
+    private String endpoint;
 
     public ReadDemo() {
         // TODO Auto-generated constructor stub
@@ -20,10 +27,22 @@ public class ReadDemo {
     
     public static void main(String[] args) {
         ReadDemo app = new ReadDemo();
+        
+        try (InputStream input = ReadDemo.class.getClassLoader().getResourceAsStream("application.properties")) {
+            Properties prop = new Properties();
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            app.endpoint=prop.getProperty("fcrepo.uri"); 
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
 
         try {
              app.run();
-
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -34,13 +53,11 @@ public class ReadDemo {
     public void run() {
         
         final FcrepoClient client = FcrepoClient.client().build();
-         
+        System.out.println("endpoint: "+ this.endpoint); 
         
         URI uri = null;
-        try {
-            //uri = new URI("http://localhost:8984/rest/dev/am/s_/ag/en/ams_agency");
-            //uri = new URI("http://aws-108-191.internal.library.cornell.edu:8080/fedora/rest/stg");
-            uri = new URI("http://aws-108-191.internal.library.cornell.edu:8080/fedora/rest/stg/am/s_/lp/s_/ams_lps_division");
+        try { 
+            uri = new URI(endpoint);
         } catch (URISyntaxException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
