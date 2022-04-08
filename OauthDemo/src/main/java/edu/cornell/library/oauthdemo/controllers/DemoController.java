9b309692ad6c08f279b4d2c22b3f84eea26761ef
John Fereira <jaf30@cornell.edu>
2022-04-08 09:50:02 -0400

    add index to controller

--- OauthDemo/src/main/java/edu/cornell/library/oauthdemo/controllers/DemoController.java
+++ OauthDemo/src/main/java/edu/cornell/library/oauthdemo/controllers/DemoController.java
@@ -9,11 +9,17 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class DemoController {
+    
+    @RequestMapping({ "/index", "/" })
+    public String index() {
+        return "index";
+    }
   
     @GetMapping("/user")
     @ResponseBody
